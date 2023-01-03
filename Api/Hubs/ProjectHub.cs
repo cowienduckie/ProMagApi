93e52fc67857deea618f0b14ac3bc7a4898a2c78
Minh Tran <minhtran.2404@outlook.com>
2023-01-03 13:22:59 +0700

    feat: Add hub methods

--- Api/Hubs/ProjectHub.cs
+++ Api/Hubs/ProjectHub.cs
@@ -8,4 +8,36 @@ public class ProjectHub : Hub
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
+
+    #region WorkItem
+    public async Task ItemAdded(Guid columnId, Guid itemId)
+    {
+        await Clients.All.SendAsync("ItemAdded", columnId, itemId);
+    }
+    public async Task ItemUpdate(Guid itemId)
+    {
+        await Clients.All.SendAsync("ItemUpdate", itemId);
+    }
+    public async Task ItemDeleted(Guid columnId, Guid itemId)
+    {
+        await Clients.All.SendAsync("ItemDeleted", columnId, itemId);
+    }
+    #endregion
+
+    #region WorkColumn
+    public async Task ColumnAdded(Guid columnId)
+    {
+        await Clients.All.SendAsync("ColumnAdded", columnId);
+    }
+
+    public async Task ColumnUpdate(Guid columnId)
+    {
+        await Clients.All.SendAsync("ColumnUpdate", columnId);
+    }
+
+    public async Task ColumnDeleted(Guid columnId)
+    {
+        await Clients.All.SendAsync("ColumnDeleted", columnId);
+    }
+    #endregion
 }
